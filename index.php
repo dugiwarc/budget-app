8c055b35e5aa5964c1c89ca71f22ed979af6011d
dugiwarc <gicubotnaru@gmail.com>
2021-03-04 14:41:21 +0100

    add test connection

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+$con = mysqli_connect("localhost", "dugiwarc", "069249335", "budget_app");
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect" . mysqli_connect_errno();
+}
+
+$query = mysqli_query($con, "INSERT INTO users VALUES(NULL,'George')");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
